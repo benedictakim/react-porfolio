58dbae4ef5a453ef8b9b955d9d9e46d492703e81
Benn <61118590+benedictakim@users.noreply.github.com>
2020-10-08 23:54:54 -0400

    starter files from class activities

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# react-porfolio
+# React Portfolio
+
+Refactoring my portfolio site https://benedictakim.github.io/
+*from using: materialize css framework and css and js 
+
+*to a portfolio using: React, react materialize, Node
+
+Advantages to refactoring to React:
+one code set for each repeating content - change in one place, publish everywhere applicable
+-header
+-footer
+-project carousel
--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
